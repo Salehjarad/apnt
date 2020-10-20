02b034e097b6945574714b006e550cd23ef9b6b4
root <salehjarad.art@gmail.com>
2020-10-20 14:17:46 +0300

    updating prisma and adding upload scaler

--- src/models/types/Post.ts
+++ src/models/types/Post.ts
@@ -1,4 +1,6 @@
-import { objectType, extendType } from '@nexus/schema';
+import { objectType, extendType, stringArg, intArg, arg } from '@nexus/schema';
+import { createWriteStream, ReadStream } from 'fs';
+import { join } from 'path';
 
 export const Post = objectType({
   name: 'Post',
@@ -6,15 +8,87 @@ export const Post = objectType({
     t.model.id();
     t.model.title();
     t.model.content();
+    t.model.imgUrl();
     t.model.author({ type: 'User' })
   }
 })
 
+interface UploadProcess {
+  stream: any,
+  filename: string;
+  mimetype: string;
+}
+
+interface ReturnUpload {
+  path?: string;
+  filename?: string;
+  mimetype?: string;
+}
+
+const uploadProcessing = ({stream, filename, mimetype}: UploadProcess): Promise<ReturnUpload> => {
+  const uploadFolderPath = join(__dirname, '..', '..', '..', 'uploades');
+  const path = `${uploadFolderPath}/${Date.now()}-${filename}`;
+  
+  return new Promise((resolve, reject) => {
+    stream
+      .pipe(createWriteStream(path))
+      .on('finish', () => resolve({ path, filename, mimetype }))
+      .on('error', reject)
+  })
+}
+
+
+export const AddNewPost = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.field('addNewPost', {
+      type: 'Post',
+      nullable: false,
+      args: {
+        title: stringArg({ required: true }),
+        content: stringArg({ required: true }),
+        by: intArg({ required: true }),
+        image: arg({ type: 'Upload', required: false })
+      },
+      resolve: async (_root, args, { prisma }) => {
+        let timeout: any;
+        let path: string | undefined = '';
+        try {
+          // const { image }  = args;
+          // if(args?.image) {
+          //   const { createReadStream, mimetype, filename, encoding } = await args?.image;
+          //   // console.log(image)
+          //   const stream = createReadStream();
+          //   const resultes = await uploadProcessing({stream, filename, mimetype})
+          //   // path = resultes?.path;
+          //   console.log(resultes?.filename)
+          // }
+          // timeout = setTimeout(() => {}, 300)
+          return await prisma.post.create({
+            data: {
+              title: args.title,
+              content: args.content,
+              imgUrl: path !== '' || path !== undefined ? path : '',
+              author: {
+                connect: {
+                  id: args.by
+                }
+              }
+            },
+          })
+
+        } catch(e) {
+          throw new Error(e);
+        }
+
+      }
+    })
+  }
+})
 
 export const PostMutation = extendType({
   type: 'Mutation',
   definition(t) {
-    t.crud.createOnePost()
     t.crud.updateOnePost()
     t.crud.deleteOnePost({
       type: 'Post',
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server-express';
 import express, { Application } from 'express';
 import { createServer } from 'http';
+import { graphqlUploadExpress } from 'graphql-upload';
 import { createContext } from './context';
 
 // schema
@@ -9,10 +10,13 @@ import schema from './schema';
 const app: Application = express();
 const PORT: number = 9090;
 
+app.use(graphqlUploadExpress({maxFieldSize: 1000000000, maxFiles: 3 }))
+
 const server = createServer(app);
 const apollo = new ApolloServer({
   schema,
-  context: createContext
+  context: createContext,
+  uploads: false
 })
 
 
